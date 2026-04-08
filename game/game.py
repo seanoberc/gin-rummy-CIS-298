fffc5de8aa1fea8b55ceea7484b93c384bad2c52
Sean Oberc <sean.oberc@techelevator.com>
2026-04-08 11:15:42 -0400

    add: a few comments to the game class re. functionality

---
+++ game/game.py
@@ -0,0 +1,13 @@
+'''
+`game` class will manage a single hand of Gin Rummy.
+- has two parameters: player1 & player 2
+- defines the behavior for a game of Gin Rummy
+'''
+
+# func deal()
+
+# func draw()
+
+# func discard()
+
+# func play() [run function]
