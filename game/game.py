06570ded107fbf61c944425fef6e806924e85db5
Sean Oberc <sean.oberc@techelevator.com>
2026-04-13 09:55:08 -0400

    add: func defs to Game class

--- game/game.py
+++ game/game.py
@@ -1,9 +1,28 @@
-import
+import pydealer
 
-# func deal()
+class Game:
+    def __init__(self):
+        self.deck = pydealer.Deck()
+        self.deck.shuffle()
+        self.discard_pile = pydealer.Stack()
+        self.player_hand = None
+        self.ai_hand = None
 
-# func draw()
+    # TODO: func deal()
+    def deal(self):
+        self.player_hand = self.deck.deal(10)
+        self.ai_hand = self.deck.deal(10)
 
-# func discard()
+        # flip one card to begin the discard pile:
+        self.discard_pile.add(self.deck.deal(1))
 
-# func play() [run function]
+    # TODO: func draw()
+    def get_top_discard(self):
+        return self.discard_pile[-1]
+
+    # TODO: func discard()
+    def draw_from_deck(self):
+        return self.deck.deal(1)
+
+    def discard(self, card):
+        self.discard_pile.add(card)
