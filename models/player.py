320cc4080f6bed03d7cc7cddb2c37d90f8fa6bb1
Sean Oberc <sean.oberc@techelevator.com>
2026-04-24 10:44:39 -0400

    create: view files fro hand, bin, and pile

--- models/player.py
+++ models/player.py
@@ -4,6 +4,10 @@ class Player:
         self.name = name
         self.hand = []
         self.score = 0
+        self.groups = {
+            "runs": [],
+            "sets": [],
+        }
 
     def set_hand(self, cards):
         self.hand = cards
@@ -16,3 +20,21 @@ class Player:
 
     def hand_size(self):
         return len(self.hand)
+
+    def move_to_group(self, card, group_name):
+        # removes the card from player's hand or any other group it might be in:
+        if card in self.hand:
+            self.hand.remove(card) # if the card is already in the hand, remove it
+
+        # remove the card from any group that it's currently in:
+        for card in self.groups[name]:
+            self.groups[name].remove(card)
+
+        self.groups[group_name].append(card) # appends card to target group
+
+    def move_to_hand(self, card):
+        for name in ("runs", "sets"):
+            if card in self.groups[name]:
+                self.groups[name].remove(card)
+            if card not in self.hand:
+                self.hand.append(card)
