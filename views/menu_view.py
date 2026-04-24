402a4eb0f301d911c598f4d2060b74844eff276f
Sean Oberc <sean.oberc@techelevator.com>
2026-04-24 18:41:24 -0400

    add: start and exit buttons to menu_view.py

--- views/menu_view.py
+++ views/menu_view.py
@@ -73,3 +73,51 @@ class MenuView:
                     self.player_name += event.unicode
 
         return None
+
+    def draw(self):
+        self.screen.fill(FELT_COLOR)
+
+        # game title
+        title = self.font_title.render("Gin Rummy", True, TITLE_COLOR)
+        self.screen.blit(title, (
+            self.window_width // 2 - title.get_width() // 2,
+            self.window_height // 3 - title.get_height() // 2
+        ))
+
+        # player-name label
+        label = self.font_label.render("Enter your name:", True, TEXT_COLOR)
+        self.screen.blit(label, (
+            self.window_width // 2 - label.get_width() // 2,
+            self.input_rect.y - 36
+        ))
+
+        # text input box:
+        input_color = INPUT_ACTIVE_COLOR if self.input_active else INPUT_COLOR
+        pygame.draw.rect(self.screen, input_color, self.input_rect, border_radius=6)
+        name_surface = self.font_input.render(self.player_name, True, (0, 0, 0))
+        self.screen.blit(name_surface, (self.input_rect.x + 10, self.input_rect.y + 10))
+
+        # cursor blinks when text input is active:
+        if self.input_active:
+            cursor_x = self.input_rect.x + 10 + name_surface.get_width() + 2
+            pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(cursor_x, self.input_rect.y + 8, 2, 28))
+
+        # start game button:
+        mx, my = pygame.mouse.get_pos()
+        start_color = BUTTON_HOVER_COLOR if self.start_rect.collidepoint(mx, my) else BUTTON_COLOR
+        pygame.draw.rect(self.screen, start_color, self.start_rect, border_radius=6)
+        start_label = self.font_button.render("Start Game", True, TEXT_COLOR)
+        self.screen.blit(start_label, (
+            self.start_rect.centerx - start_label.get_width() // 2,
+            self.start_rect.centery - start_label.get_height() // 2
+        ))
+
+        # exit program button:
+        exit_color = BUTTON_HOVER_COLOR if self.exit_rect.collidepoint(mx, my) else BUTTON_COLOR
+        pygame.draw.rect(self.screen, exit_color, self.exit_rect, border_radius=6)
+        exit_label = self.font_button.render("Exit", True, TEXT_COLOR)
+        self.screen.blit(exit_label, (self.exit_rect.centerx - exit_label.get_width() // 2,
+            self.exit_rect.centery - exit_label.get_height() // 2
+        ))
+
+        pygame.display.flip()
