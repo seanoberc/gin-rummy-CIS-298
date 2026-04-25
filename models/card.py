9bc662488f8daff3a08a06c10ca4f6da7a939dc5
Sean Oberc <sean.oberc@techelevator.com>
2026-04-25 12:09:43 -0400

    debug and fix: got the alternate card faces to work by hard-coding; going to implement a menu option next

--- models/card.py
+++ models/card.py
@@ -1,4 +1,6 @@
 import pygame
+from models import card_style
+from models.spritesheet import SpriteSheet
 
 CARD_WIDTH = 80
 CARD_HEIGHT = 110
@@ -17,25 +19,34 @@ RANK_TO_FILE = {
     "Jack": "Jack", "Queen": "Queen", "King": "King",
 }
 
-# Card class extends Pygame's Sprite class
+# Card class extends PyGame's Sprite class
 class Card(pygame.sprite.Sprite):
+    _spritesheet = None     # spritesheet shared across all cards
+
     def __init__(self, rank, suit):
         super().__init__()  # constructor for the parent class (which is now `Sprite`)
         self.rank = rank
         self.suit = suit
 
-        path = self._image_path()
-        image = pygame.image.load(path).convert_alpha()
+        # path = self._image_path()
+        # image = pygame.image.load(path).convert_alpha()
+        image = self._load_image()
         self.image = pygame.transform.smoothscale(image, (CARD_WIDTH, CARD_HEIGHT)) # Pygame uses `Surface` for the canvas
-        self.rect = self.image.get_rect()       # Pygame's 'rect` stores the card position on the screen
+        self.rect = self.image.get_rect()       # PyGame's 'rect` stores the card position on the screen
+
+    def _load_image(self):
+        if card_style.current_style == "classic":
+            if Card._spritesheet is None:
+                Card._spritesheet = SpriteSheet()
+            return Card._spritesheet.get_card_face(self.rank, self.suit)
+        else:
+            path = self._image_path()
+            return pygame.image.load(path).convert_alpha()
 
     def _image_path(self):
         suit_name = SUIT_TO_FILE[self.suit]
         rank_name = RANK_TO_FILE[self.rank]
         return f"assets/images/sprites/{suit_name}_{rank_name}_black.png"
-
-        # Draw the rank and suit onto the card image
-        # self._draw_card()
 
     def __repr__(self):
         return f"{self.rank} of {self.suit}"
---
+++ models/card_style.py
@@ -0,0 +1,6 @@
+# globally tracks selected card style
+#       (putting this here to avoid any circular loading)
+# current options: "default" or "classic"
+
+current_style = "default"
+current_back = "blue"
